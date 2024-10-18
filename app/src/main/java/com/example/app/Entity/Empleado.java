a8904a5cbf0c01c9dd38f162dd4f6943fdfac98c
Jhonatan Chaparro <167357134+Jhoanatanchaparro@users.noreply.github.com>
2024-10-18 13:39:39 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/app/Entity/Empleado.java
@@ -0,0 +1,91 @@
+package com.example.app.Entity;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "Empleados")
+@NoArgsConstructor
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String telefono;
+    private String correoElectronico;
+    private String direccionResidencia;
+    private String nombre;
+    private String apellido;
+    private double salario;
+    @ManyToOne
+    @JoinColumn(name = "rol_id")
+    private Rol rol;
+
+    public Empleado(String nombre, String apellido, String telefono, String correoElectronico,
+            String direccionResidencia, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.direccionResidencia = direccionResidencia;
+        this.salario = salario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccionResidencia() {
+        return direccionResidencia;
+    }
+
+    public void setDireccionResidencia(String direccionResidencia) {
+        this.direccionResidencia = direccionResidencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
---
+++ app/src/main/java/com/example/app/Repository/RolRepository.java
@@ -0,0 +1,9 @@
+package com.example.app.Repository;
+
+import com.example.app.Entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository <Rol, Long> {
+
+    
+}
