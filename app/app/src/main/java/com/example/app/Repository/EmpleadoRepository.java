64fa82cc3b87f396943cd2877dbe76270ec4acf5
Jhonatan Chaparro <167357134+Jhoanatanchaparro@users.noreply.github.com>
2024-10-18 13:38:23 -0500

    Delete app/app directory

--- app/app/src/main/java/com/example/app/Repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.app.Entity.Empleado;
-
-public interface EmpleadoRepository extends JpaRepository < Empleado, Long> {
-
-}
