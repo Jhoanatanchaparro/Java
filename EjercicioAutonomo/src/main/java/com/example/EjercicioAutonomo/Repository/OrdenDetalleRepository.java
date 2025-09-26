d98db8b5b06fdd2afa73d7f7687e4c4c6f84f0c7
Jhonatan Chaparro <jhonatand_chaparro@soy.sena.edu.co>
2025-09-26 10:04:05 -0500

    ...

--- EjercicioAutonomo/src/main/java/com/example/EjercicioAutonomo/Repository/OrdenDetalleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EjercicioAutonomo.Repository;
-
-import com.example.EjercicioAutonomo.Entity.Orden_Detalle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrdenDetalleRepository extends JpaRepository<Orden_Detalle, Long> {
-}
