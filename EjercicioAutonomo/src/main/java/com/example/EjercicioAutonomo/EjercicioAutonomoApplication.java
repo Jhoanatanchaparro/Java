367b3f106d7145b5c44f9e7bf1c254e06bc3da1f
Jhonatan Chaparro <jhonatand_chaparro@soy.sena.edu.co>
2025-09-30 13:51:01 -0500

    ...

--- EjercicioAutonomo/src/main/java/com/example/EjercicioAutonomo/EjercicioAutonomoApplication.java
+++ EjercicioAutonomo/src/main/java/com/example/EjercicioAutonomo/EjercicioAutonomoApplication.java
@@ -2,8 +2,10 @@ package com.example.EjercicioAutonomo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient
 public class EjercicioAutonomoApplication {
 
 	public static void main(String[] args) {
